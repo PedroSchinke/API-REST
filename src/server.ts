de14e2911a87e2f6ecbe0a8199eab97c6c7be0b6
Pedro Schinke <pedro.schinke@outlook.com>
2023-10-26 01:26:27 -0300

    added e2e test

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,5 @@
-import fastify from 'fastify'
-import { knex } from './database'
-import crypto from 'node:crypto'
-import { env } from '../src/env/index'
-
-const app = fastify()
-
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    // .insert({
-    //   id: crypto.randomUUID(),
-    //   title: 'Transação de teste',
-    //   amount: 1000,
-    // })
-    // .returning('*')
-    .where('amount', 1000)
-    .select('*')
-
-  return transaction
-})
+import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
---
+++ test/transactions.spec.ts
@@ -0,0 +1,124 @@
+import { afterAll, beforeAll, beforeEach, expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+import { describe } from 'node:test'
+import { execSync } from 'child_process'
+
+describe('Transaction routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  test('user can create new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201) // or expect(response.statusCode).toEqual(201)
+  })
+
+  test('user can list all transactions', async () => {
+    const createTransactionsResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionsResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
+
+  test('user can list individual transaction', async () => {
+    const createTransactionsResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionsResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionsResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionsResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  test('user can get summary', async () => {
+    const createTransactionsResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionsResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
+  })
+})
+
+// test('o usuário consegue criar uma nova trasação', () => {
+//   fazer a chamada http para criar uma nova transação
+
+//   const responseStatusCode = 201
+
+//   expect(responseStatusCode).toEqual(201)
+// })
